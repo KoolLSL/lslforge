b78179bdeee44e911b1812a4d3394ebc551d8ce2
KoolLSL <6314636+KoolLSL@users.noreply.github.com>
2023-01-22 18:27:25 +0000

    Update all_compile_haskell_win32.bat
    Merged previous codegen.bat, copy_win32.bat in this single bat

--- lslforge/all_compile_haskell_win32.bat
+++ lslforge/all_compile_haskell_win32.bat
@@ -1,28 +1,33 @@
 @echo off
-pushd "%~dp0"
-
-echo.
+echo Started %date% %time%
 echo ---- Compiling Haskell...
 echo.
 cd haskell
-stack install --executable-stripping
+rem remove Stack temp directory
+rmdir ".stack-work" /S /Q
 
+rem --copy-bins: copy resulting LSLForge.exe to local binary directory %UserProfile%\AppData\Roaming\local\bin\ (see stack path --local-bin)
+rem --executable-stripping: executable stripping for TARGETs and all its dependencies.
+rem --force-dirty: force rebuild of packages even when it doesn't seem necessary based on file dirtiness.
+rem --reconfigure: force reconfiguration even when it doesn't seem necessary based on file dirtiness.
+stack install --executable-stripping --reconfigure --copy-bins
+@echo off
 cd ..
 
 echo.
 echo ---- Generating Java Code...
 echo.
-call codegen.bat
-
-echo.
-echo ---- Copying Haskell Executable...
-echo.
-call copy_win32.bat
+pushd "%~dp0"
+echo on
+del eclipse\lslforge\src\lslforge\generated\*.java
+%UserProfile%\AppData\Roaming\local\bin\LSLForge.exe _CodeGen_ eclipse\lslforge\src\lslforge\generated lslforge.generated
+@echo off
+popd
 
 echo.
 echo ---- Cleaning Eclipse Update-Site Directory...
 echo.
 call eclipse\update-site\clean.bat
-
-rem timeout /t 60
 popd
+
+pause
