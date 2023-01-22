516b4c746485c7d42c27399347a3a188e6e684a2
KoolLSL <6314636+KoolLSL@users.noreply.github.com>
2023-01-22 18:56:47 +0000

    Delete codegen.bat
    file was merged in all_compile_haskell_win32.bat

--- lslforge/codegen.bat
+++
@@ -1,9 +0,0 @@
-pushd "%~dp0"
-
-rem ren eclipse\lslforge\src\lslforge\generated\Tuple2.java Tuple2.save
-rem ren eclipse\lslforge\src\lslforge\generated\Tuple3.java Tuple3.save
-del eclipse\lslforge\src\lslforge\generated\*.java
-rem ren eclipse\lslforge\src\lslforge\generated\*.save *.java
-
-%UserProfile%\AppData\Roaming\local\bin\LSLForge.exe _CodeGen_ eclipse\lslforge\src\lslforge\generated lslforge.generated
-popd
